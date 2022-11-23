d7a16c38ddd5d43cf5c3f6a5e479a3f895b28677
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2022-11-23 11:17:38 -0500

    Add unit tests for pinot-json-log.

---
+++ pinot-plugins/pinot-input-format/pinot-json-log/src/test/java/org/apache/pinot/plugin/inputformat/json/JSONLogRecordExtractorTest.java
@@ -0,0 +1,143 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.pinot.plugin.inputformat.json;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import org.apache.pinot.plugin.inputformat.json.function.CLPUDFs;
+import org.apache.pinot.spi.data.readers.GenericRow;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+
+public class JSONLogRecordExtractorTest {
+  @Test
+  void testCLPEncoding() {
+    JSONLogMessageDecoder messageDecoder = new JSONLogMessageDecoder();
+    Map<String, String> props = new HashMap<>();
+    props.put("fieldsForClpEncoding", "message");
+    props.put("jsonDataField", "jsonData");
+    Set<String> fieldsToRead = new HashSet<>();
+    fieldsToRead.add("timestamp");
+    fieldsToRead.add("message");
+    try {
+      messageDecoder.init(props, fieldsToRead, null);
+    } catch (Exception e) {
+      fail(e.toString());
+    }
+
+    GenericRow row = new GenericRow();
+    String message = "Started job_123 on node-987 with 4 cores, 8 threads with 51.4% memory used.";
+    messageDecoder.decode(
+        ("{\"timestamp\":10,\"message\":\"" + message + "\"}").getBytes(StandardCharsets.ISO_8859_1), row);
+    assertEquals(row.getValue("timestamp"), 10);
+    try {
+      String logtype = (String) row.getValue("message_logtype");
+      assertNotEquals(logtype, null);
+      String[] dictionaryVars = (String[]) row.getValue("message_dictionaryVars");
+      assertNotEquals(dictionaryVars, null);
+      Long[] encodedVars = (Long[]) row.getValue("message_encodedVars");
+      assertNotEquals(encodedVars, null);
+      long[] encodedVarsAsPrimitives = Arrays.stream((Long[]) row.getValue("message_encodedVars"))
+          .mapToLong(Long::longValue)
+          .toArray();
+
+      String decodedMessage = CLPUDFs.clpDecode((String) row.getValue("message_logtype"),
+          (String[]) row.getValue("message_dictionaryVars"), encodedVarsAsPrimitives);
+      assertEquals(message, decodedMessage);
+
+      assertTrue(CLPUDFs.matchEncodedIntVars("*4*", logtype, encodedVarsAsPrimitives));
+      assertFalse(CLPUDFs.matchEncodedIntVars("*9*", logtype, encodedVarsAsPrimitives));
+      assertFalse(CLPUDFs.matchEncodedIntVars("*1.4*", logtype, encodedVarsAsPrimitives));
+
+      assertTrue(CLPUDFs.matchEncodedFloatVars("*1.4*", logtype, encodedVarsAsPrimitives));
+      assertFalse(CLPUDFs.matchEncodedFloatVars("*5.1*", logtype, encodedVarsAsPrimitives));
+      assertFalse(CLPUDFs.matchEncodedFloatVars("*8*", logtype, encodedVarsAsPrimitives));
+    } catch (ClassCastException e) {
+      fail(e.toString());
+    } catch (IOException e) {
+      fail("Could not decode message with CLP.");
+    }
+  }
+
+  @Test
+  void testNestedFieldExtraction() {
+    JSONLogMessageDecoder messageDecoder = new JSONLogMessageDecoder();
+    Map<String, String> props = new HashMap<>();
+    Set<String> fieldsToRead = new HashSet<>();
+    fieldsToRead.add("timestamp");
+    fieldsToRead.add("parent.child");
+    try {
+      messageDecoder.init(props, fieldsToRead, null);
+    } catch (Exception e) {
+      fail(e.toString());
+    }
+
+    GenericRow row = new GenericRow();
+    messageDecoder.decode(
+        ("{\"timestamp\":10,\"parent\":{\"child\":\"value\"}}").getBytes(StandardCharsets.ISO_8859_1), row);
+    assertEquals(row.getValue("timestamp"), 10);
+    // Check that the field was flattened
+    assertEquals(row.getValue("parent.child"), "value");
+    // Check that no parent key was added
+    assertNull(row.getValue("parent"));
+  }
+
+  @Test
+  void testJSONDataField() {
+    JSONLogMessageDecoder messageDecoder = new JSONLogMessageDecoder();
+    Map<String, String> props = new HashMap<>();
+    props.put("jsonDataField", "jsonData");
+    Set<String> fieldsToRead = new HashSet<>();
+    fieldsToRead.add("timestamp");
+    try {
+      messageDecoder.init(props, fieldsToRead, null);
+    } catch (Exception e) {
+      fail(e.toString());
+    }
+
+    GenericRow row = new GenericRow();
+    messageDecoder.decode(
+        ("{\"timestamp\":10,\"parent\":{\"child\":\"value\"}}").getBytes(StandardCharsets.ISO_8859_1), row);
+    assertEquals(row.getValue("timestamp"), 10);
+    // Check that fields not part of the schema weren't added
+    assertNull(row.getValue("parent.child"));
+    assertNull(row.getValue("parent"));
+    assertNull(row.getValue("child"));
+    assertNull(row.getValue("parent"));
+
+    try {
+      assertNotNull(row.getValue("jsonData"));
+      Map<String, Object> jsonData = (Map<String, Object>) row.getValue("jsonData");
+      assertEquals(jsonData.size(), 1);
+
+      Map<String, Object> parent = (Map<String, Object>) jsonData.get("parent");
+      assertNotNull(parent);
+      assertEquals(parent.get("child"), "value");
+    } catch (ClassCastException e) {
+      fail(e.toString());
+    }
+  }
+}
