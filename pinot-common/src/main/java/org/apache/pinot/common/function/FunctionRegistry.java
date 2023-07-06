192e4382fa170a3b5929b76d7b3b93d0ebda24d7
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2023-07-06 07:57:35 -0400

    Define clpDecode and clpEncodedVarsMatch as scalar functions with no implementations so they pass the existence test.

--- pinot-common/src/main/java/org/apache/pinot/common/function/FunctionRegistry.java
+++ pinot-common/src/main/java/org/apache/pinot/common/function/FunctionRegistry.java
@@ -205,5 +205,23 @@ public class FunctionRegistry {
     public static boolean jsonMatch(String text, String pattern) {
       throw new UnsupportedOperationException("Placeholder scalar function, should not reach here");
     }
+
+    @ScalarFunction(names = {"clpDecode", "clp_decode"}, isPlaceholder = true)
+    public static Object clpDecode(String logtypeFieldName, String dictVarsFieldName, String encodedVarsFieldName) {
+      throw new UnsupportedOperationException("Placeholder scalar function, should not reach here");
+    }
+
+    @ScalarFunction(names = {"clpDecode", "clp_decode"}, isPlaceholder = true)
+    public static Object clpDecode(String logtypeFieldName, String dictVarsFieldName, String encodedVarsFieldName,
+        String defaultValue) {
+      throw new UnsupportedOperationException("Placeholder scalar function, should not reach here");
+    }
+
+    @ScalarFunction(names = {"clpEncodedVarsMatch", "clp_encoded_vars_match"}, isPlaceholder = true)
+    public static Object clpEncodedVarsMatch(String logtypeFieldName, String encodedVarsFieldName,
+        String serializedVarPlaceholders, String serializedVarWildcardQueries,
+        String serializedVarWildcardQueryEndIndexes) {
+      throw new UnsupportedOperationException("Placeholder scalar function, should not reach here");
+    }
   }
 }
